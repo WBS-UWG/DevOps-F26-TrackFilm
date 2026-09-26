3e1b3157151c8479c7e71c96ae08719a3e3614b2
WilliamBSITT <william.sitt@epitech.eu>
2026-09-26 15:17:19 -0400

    [ADD] Create the main page for view items - Edit the MainWindow.fxml layout in order to have 2separate list for films & series - Create some films & series in MainWindow.java to test the UI layout - Disable the AddItemWindow.java (will be implement later) - Do a simple WatchedItem.java class to create a film with just a name & a director

--- code/Film_tracker/src/main/java/edu/westga/comp4420/film_tracker/model/WatchedItem.java
+++ code/Film_tracker/src/main/java/edu/westga/comp4420/film_tracker/model/WatchedItem.java
@@ -1,18 +1,41 @@
 package edu.westga.comp4420.film_tracker.model;
 
 /**
- * Stores basic information for a single item in a grocery list.
+ * Stores basic information for a single watched film or series.
  *
  * @author	Comp 4420
  * @version Fall 2026
  */
 public final class WatchedItem {   
 
-	private String title;
-    private String director;
+	private final String title;
+    private final String director;
     
     public WatchedItem(String title, String director) {
         this.title = title;
         this.director = director;
+    }
+
+    /**
+     * Gets the item's title.
+     *
+     * @return the title
+     */
+    public String getTitle() {
+        return this.title;
+    }
+
+    /**
+     * Gets the item's director or creator.
+     *
+     * @return the director or creator
+     */
+    public String getDirector() {
+        return this.director;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + " (" + this.director + ")";
     }
 }
--- code/Film_tracker/src/main/java/edu/westga/comp4420/film_tracker/view/codebehind/AddItemWindow.java
+++ code/Film_tracker/src/main/java/edu/westga/comp4420/film_tracker/view/codebehind/AddItemWindow.java
@@ -1,28 +1,28 @@
-package edu.westga.comp4420.film_tracker.view.codebehind;
+// package edu.westga.comp4420.film_tracker.view.codebehind;
 
-import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
-import java.util.List;
+// import javafx.fxml.FXML;
+// import javafx.scene.layout.AnchorPane;
+// import java.util.List;
 
-import edu.westga.comp4420.film_tracker.model.WatchedItem;
+// import edu.westga.comp4420.film_tracker.model.WatchedItem;
 
-/**
- * CodeBehind To Handle Processing for the AddItemWindow
- *
- * @author	Comp 4420
- * @version Fall 2026
- */
-public class AddItemWindow {   
-	@FXML private AnchorPane guiPane;
+// /**
+//  * CodeBehind To Handle Processing for the AddItemWindow
+//  *
+//  * @author	Comp 4420
+//  * @version Fall 2026
+//  */
+// public class AddItemWindow {   
+// 	@FXML private AnchorPane guiPane;
 	
-	private List<WatchedItem> watchedItems;
+// 	private List<WatchedItem> watchedItems;
 	
-	public void setItemList(List<WatchedItem> watchedItems) {
-		this.watchedItems = watchedItems;
-	}
+// 	public void setItemList(List<WatchedItem> watchedItems) {
+// 		this.watchedItems = watchedItems;
+// 	}
 	
-	@FXML
-	void initialize() {
-		assert this.guiPane != null : "fx:id=\"guiPane\" was not injected: check your FXML file 'MainWindow.fxml'.";
-	}
-}
+// 	@FXML
+// 	void initialize() {
+// 		assert this.guiPane != null : "fx:id=\"guiPane\" was not injected: check your FXML file 'MainWindow.fxml'.";
+// 	}
+// }
